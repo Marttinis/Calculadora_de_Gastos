b8c13799263ac436114e990c2894bc1c517a9a4f
Gabriel Martins <gabrielmartins23445@gmail.com>
2025-01-28 13:35:41 -0300

    alterações do css do formulario

--- src/Components/Formulario.jsx
+++ src/Components/Formulario.jsx
@@ -1,17 +1,21 @@
 import '../assets/css/Formulario.css'
 function Formulario() {
+    
     return (
         <div id='formulario'>
             <form>
-                <h1>Calculadora</h1>
-                <label htmlFor="Nome">Nome: </label>
-                <input type="text" placeholder="Digite seu nome" required />
+                <h1>Calculadora de Gastos 🧮</h1>
+                <label htmlFor="Nome">Salario Total: </label>
+                <input type="number" id='Salario' placeholder="Digite o valor do seu Salario" required />
 
-                <label htmlFor="Nome">Gastos: </label>
-                <input type="text" placeholder="Quanto gastou nisto ?" required />
+                <label htmlFor="Nome">Com o que você gastou? </label>
+                <input type="text" id='nome' placeholder="" required />
+
+                <label htmlFor="Nome">Valor: </label>
+                <input type="number" id='valor' placeholder="Quanto gastou nisto ?" required />
 
                 <div id='botao'>
-                    <button>Enviar</button>
+                    <button href="">Enviar</button>
                 </div>
 
             </form>
---
+++ src/Components/grafico.jsx
@@ -0,0 +1,9 @@
+function Grafico() {
+    return (
+        <>
+        <h1>Aqui vai ser o grafico</h1>
+        </>
+    );
+}
+
+export default Grafico;
